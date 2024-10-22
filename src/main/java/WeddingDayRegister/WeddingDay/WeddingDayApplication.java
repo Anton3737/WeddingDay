8a9d7f8920a641f8691debd12be3fb0dc2997b41
Anton <BarashASUA@gmail.com>
2024-10-23 00:17:45 +0300

    pipes tests

--- src/main/java/WeddingDayRegister/WeddingDay/WeddingDayApplication.java
+++ src/main/java/WeddingDayRegister/WeddingDay/WeddingDayApplication.java
@@ -3,10 +3,8 @@ package WeddingDayRegister.WeddingDay;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootApplication
-@ActiveProfiles("test")
 public class WeddingDayApplication {
     public static void main(String[] args) {
         SpringApplication.run(WeddingDayApplication.class, args);
