66c461bb0a4a1f770cab61dec45beca527320a46
Anton <BarashASUA@gmail.com>
2025-08-06 22:44:01 +0300

    pipes tests

--- src/main/java/WeddingDayRegister/WeddingDay/controller/InvitationController.java
+++ src/main/java/WeddingDayRegister/WeddingDay/controller/InvitationController.java
@@ -8,6 +8,6 @@ public class InvitationController {
 
     @GetMapping("/")
     public String showInvitation() {
-        return "invitation"; // This should match the name of your HTML file without the extension
+        return "invitation"; //
     }
 }
